3502487f6dd9b31e83f4ebd339ff9961a64e10da
ValdesAGB <valdesagbokoni01@gmail.com>
2022-12-18 09:55:01 +0100

    Mise à jour de Cart.js: Ajout de bouton de retrait du panier

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
 function Cart({ cart, setCart, total }) {
+  // fonction pour retirer un éléments du panier
+  function withdrawCart(name, price) {
+    const currentClothesAmount = cart.find((cloth) => cloth.name === name)
+    if (currentClothesAmount) {
+      const cartFilteredCurrentClothes = cart.filter(
+        (plant) => plant.name !== name
+      )
+      if (currentClothesAmount.amount === 1) {
+        setCart([...cartFilteredCurrentClothes])
+      } else {
+        setCart([
+          ...cartFilteredCurrentClothes,
+          { name, price, amount: currentClothesAmount.amount - 1 },
+        ])
+      }
+    }
+  }
+
   return (
     <React.Fragment>
       <i
@@ -35,23 +53,35 @@ function Cart({ cart, setCart, total }) {
             {cart.length > 0 ? (
               <div className="modal-body text-black">
                 {cart.map(({ name, price, amount }, index) => (
-                  <div key={`${name}-${index}`}>
-                    {name} : {price} € x {amount}
+                  <div key={`${name}-${index}`} className="row">
+                    <div className="col-10">
+                      {name} : {price} € x {amount}
+                    </div>
+                    <div className=" col-1">
+                      <i
+                        className="bi bi-x-lg btn text-danger fs-5"
+                        onClick={() => withdrawCart(name, price)}
+                      ></i>
+                    </div>
                   </div>
                 ))}
               </div>
             ) : (
               <div className="modal-body text-black">Votre panier est vide</div>
             )}
-            <div className="modal-footer">
-              <h5 className="fw-light">Total : {total} € </h5>
-              <button
-                type="button"
-                className="btn btn-danger fw-light"
-                onClick={() => setCart([])}
-              >
-                Vider le panier
-              </button>
+            <div className="modal-footer ">
+              <span className="mx-4">
+                <h5 className="fw-light">Total : {total} € </h5>
+              </span>
+              <span>
+                <button
+                  type="button"
+                  className="btn btn-danger fw-light"
+                  onClick={() => setCart([])}
+                >
+                  Vider le panier
+                </button>
+              </span>
             </div>
           </div>
         </div>
--- src/components/Header/Header1.js
+++ src/components/Header/Header1.js
@@ -8,15 +8,21 @@ function Header1() {
         className="row text-white fw-light"
         style={{ backgroundColor: '#0063d2' }}
       >
-        <ul className="col list-inline g-2 ms-2">
-          <li className="list-inline-item">Français</li>
-          <li className="list-inline-item mx-5">FCFA</li>
-          <li className="list-inline-item">{welcomeMessage}</li>
+        <ul className="col col-sm- list-inline g-2 ms-2">
+          <li className="list-inline-item d-none d-md-inline-block">
+            Français
+          </li>
+          <li className="list-inline-item mx-5 d-none d-lg-inline-block  ">
+            EURO
+          </li>
+          <li className="list-inline-item ms-md-2 ms-lg-0">{welcomeMessage}</li>
         </ul>
 
-        <ul className="col g-2 list-inline d-flex justify-content-end me-2">
-          <li className="list-inline-item me-5">Mon compte</li>
-          <li className="list-inline-item">Liste des souhaits (0) </li>
+        <ul className="col col-sm-6 g-2 list-inline d-flex justify-content-end me-2">
+          <li className="list-inline-item me-md-5 me-sm-0">Mon compte</li>
+          <li className="list-inline-item d-none d-md-inline-block">
+            Liste des souhaits (0){' '}
+          </li>
         </ul>
       </section>
     </React.Fragment>
